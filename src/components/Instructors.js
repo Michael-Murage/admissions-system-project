15526b0198eda3415b8711fd085625f82050d64f
Michael <mickeymurage@gmail.com>
2022-09-07 18:40:29 +0300

    added instructors display file

---
+++ src/components/Instructors.js
@@ -0,0 +1,10 @@
+import useFetchData from "../hooks/fetchData"
+
+function Instructors() {
+  const [url, setUrl, fetchData] = useFetchData()
+  return (
+    <div>instructors</div>
+  )
+}
+
+export default Instructors
