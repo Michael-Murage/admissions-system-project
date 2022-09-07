0464bb6db431224ba57856ad481c261fc42e5316
Michael <mickeymurage@gmail.com>
2022-09-07 18:42:45 +0300

    added a few other components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import '../App.css';
+import LandingPage from './LandingPage';
 
 function App() {
   return (
     <>
-    
+      <LandingPage/>
     </>
   )
 }
---
+++ src/components/LandingPage.js
@@ -0,0 +1,17 @@
+
+function LandingPage() {
+  return (
+    <div>
+      <div id="icon">
+        <h1 id="morre">M<span id="big-o">O</span>RRE</h1>
+        <p id="icon-desc">INSTITUTION OF THE ART OF CODING</p>
+      </div>
+      <h1 id="yourself">BE YOURSELF</h1>
+      <p>
+        Located in the heart of Nairobi. Morre institute aims at maintaing the tradition of providing individuals 
+      </p>
+    </div>
+  )
+}
+
+export default LandingPage
